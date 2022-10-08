893b9369296d1c0c0e7ff39555b580298fd32610
shivii011 <shiv011kush011@gamil.com>
2022-10-08 23:21:36 +0530

    changing layout of card

---
+++ src/components/shared/BuyButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {FaCartPlus} from 'react-icons/fa';
+function BuyButton({className ,onClick=()=>{}}) {
+  return (
+    <div><FaCartPlus onClick={onClick} className={`${className}  text-yellow-400 cursor-pointer text-2xl  hover:animate-bounce hover:text-violet-500 hover:transition-all`}/></div>
+  )
+}
+
+export default BuyButton
--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -4,18 +4,19 @@ import FavouriteButton from './FavouriteButton';
 import img from './mob-image.jpg'
 function Card() {
   return (
-    <div className='h-[20rem] w-[14rem] border hover:shadow-lg]'>
-      <p>its a mobile</p>
+<a href='' className='group'>
+<div  className='h-[20rem] w-[14rem] border hover:shadow-lg'>
+     <div className=''> <img className=' hover:object-center hover:border-solid hover:border-2 hover:border-yellow-400'src={img} alt='' /></div>
       <p className='text-lg text-blue-500 font-semibold'>Name Of Product</p>
-      <img className='h-[13rem] m-1 justify-center hover:h-[13.05rem] hover:w-[13.05rem]'  src={img} alt='' />
-      <div className='flex justify-between m-3'>
+      <div className='flex justify-between m-3 opacity-0 hover:opacity-100 '>
       <div><p className='text-lg text-blue-500'>$100.00</p></div>
-      <div className='flex  '>
-       <FavouriteButton className='mr-2 '/>
+      <div className='flex '>
+       <FavouriteButton className='mr-2'/>
        <BuyButton className=''/>
         </div>
       </div>
     </div>
+</a>
   )
 }
 
