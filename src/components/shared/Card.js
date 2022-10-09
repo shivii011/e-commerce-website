bc6b779e5b9fdf09d373df72b1d4ac061446a416
shivii011 <shiv011kush011@gamil.com>
2022-10-09 21:50:34 +0530

    [sk]-checking for error

--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -3,28 +3,43 @@ import BuyButton from './BuyButton';
 import FavouriteButton from './FavouriteButton';
 import img from '../../assets/mob-image.jpg';
 import Data from '../../utils/data';
-function Card() {
-  const data=[
-    {'ItemName':'Laptop',
-    'Price':'$600'},
-  ]
-const listItems = data.map((element)=>{return(<div className='h-[20rem] w-[14rem] group'>
-<div className=''> <img className=' hover:object-center hover:border-solid hover:border-2 group-hover:border-yellow-400' src={img} alt='' /></div>
-<div> <p className='text-lg text-blue-500 font-semibold'>{element.ItemName}</p>
-  <div className='flex justify-between m-3 opacity-0 group-hover:opacity-100 '>
-    <p className='text-lg text-blue-500'>{element.Price}</p>
-    <div className='flex '>
-      <FavouriteButton className='mr-2' />
-      <BuyButton className='' />
-    </div>
-  </div>
-</div>
-</div>
-)})
-  return (
- <div className='flex'>{listItems}</div>
-  )
+// function Card() {
+// const listItem= Data.map((element)=>{<div className='h-[20rem] w-[14rem] group'>
+// <div className=''> <img className=' hover:object-center hover:border-solid hover:border-2 group-hover:border-yellow-400' src={img} alt='' /></div>
+// <div> <p className='text-lg text-blue-500 font-semibold'>{element.ItemName}</p>
+//   <div className='flex justify-between m-3 opacity-0 group-hover:opacity-100 '>
+//     <p className='text-lg text-blue-500'>{element.Price}</p>
+//     <div className='flex '>
+//       <FavouriteButton className='mr-2' />
+//       <BuyButton className='' />
+//     </div>
+//   </div>
+// </div>
+// </div>}
+// )
+//   return (
+//     <div>{listItem}</div>
+//   )
   
+// }
+
+
+function Card() {
+  return (
+    <div>
+ {Data.map((data) => (
+   <div>
+   <div className="m-auto">
+       <div>{data.itemName}</div>
+       <div>{data.price} </div>
+   </div>
+   </div>
+                
+            
+        ))}
+    </div>
+  )
 }
+
 
 export default Card
