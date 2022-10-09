1c6ed53aad1e62e5482bc41054a48f18e54eea47
shivii011 <shiv011kush011@gamil.com>
2022-10-09 09:59:39 +0530

    boumce effect on click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Card from './components/shared/Card';
 import FavouriteButton from './components/shared/FavouriteButton';
-
 function App() {
   return (
     <div className="App">
      E commerce
-     <Card/>
 <FavouriteButton/>
     </div>
   );
