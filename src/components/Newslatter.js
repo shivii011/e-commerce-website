e874aecf55dece980178b9a65f1ba131de3c56a8
shivii011 <shiv011kush011@gamil.com>
2022-10-10 00:51:47 +0530

    [sk]-updating bg color

--- src/components/Newslatter.js
+++ src/components/Newslatter.js
@@ -8,7 +8,7 @@ function Newslatter() {
                 <div className='col-span-2'>...and receive <strong className=' text-lg'>$10 coupon for first shopping</strong></div></div>
 
             <div className='flex col-span-4'>  <input type="email" id="email" class=" w-3/5 h-10 rounded-r-xl rounded-full text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block " placeholder="john.doe@company.com" required />
-                <Button className={'px-1 py-0 h-10 rounded-r-2xl rounded-full border-none bg-slate-900 text-yellow-400 hover:text-slate-900'} value='Sign Up' />
+                <Button className={'bg-slate-900 px-1 py-0 h-10 rounded-r-2xl rounded-full border-none  text-yellow-400 hover:text-slate-900'} value='Sign Up' />
             </div>
         </div>
     )
