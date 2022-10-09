669fe4daee177691553f53811e569a1058b694e1
shivii011 <shiv011kush011@gamil.com>
2022-10-09 17:38:37 +0530

    [sk ]-update

--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import BuyButton from './BuyButton';
 import FavouriteButton from './FavouriteButton';
-import img from './mob-image.jpg'
+import img from '../../assets/mob-image.jpg';
+import Data from '../../utils/data';
 function Card() {
   const data=[
     {'ItemName':'Laptop',
     'Price':'$600'},
   ]
-const listItems = data.map((element)=>{return(<div className='h-[20rem] w-[14rem] group '>
+const listItems = data.map((element)=>{return(<div className='h-[20rem] w-[14rem] group'>
 <div className=''> <img className=' hover:object-center hover:border-solid hover:border-2 group-hover:border-yellow-400' src={img} alt='' /></div>
 <div> <p className='text-lg text-blue-500 font-semibold'>{element.ItemName}</p>
   <div className='flex justify-between m-3 opacity-0 group-hover:opacity-100 '>
