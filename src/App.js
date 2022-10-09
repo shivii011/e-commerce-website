d7139201cc89d2bd6beab48ed199d48784cc6745
shivii011 <shiv011kush011@gamil.com>
2022-10-09 12:05:33 +0530

    [sk]-using map arrey of object and map it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
 import Card from './components/shared/Card';
-import FavouriteButton from './components/shared/FavouriteButton';
-
 function App() {
   return (
     <div className="App">
      E commerce
      <Card/>
-<FavouriteButton/>
     </div>
   );
 }
--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -3,21 +3,33 @@ import BuyButton from './BuyButton';
 import FavouriteButton from './FavouriteButton';
 import img from './mob-image.jpg'
 function Card() {
-  return (
-<a href='' className='group'>
-<div  className='h-[20rem] w-[14rem] border hover:shadow-lg'>
-     <div className=''> <img className=' hover:object-center hover:border-solid hover:border-2 hover:border-yellow-400'src={img} alt='' /></div>
-      <p className='text-lg text-blue-500 font-semibold'>Name Of Product</p>
-      <div className='flex justify-between m-3 opacity-0 hover:opacity-100 '>
-      <div><p className='text-lg text-blue-500'>$100.00</p></div>
-      <div className='flex '>
-       <FavouriteButton className='mr-2'/>
-       <BuyButton className=''/>
-        </div>
-      </div>
+  const data=[
+    {'ItemName':'Laptop',
+    'Price':'$600'},
+    {'ItemName':'key Board',
+    'Price':'$10'},
+    {'ItemName':'mouce',
+    'Price':'$8'},
+    {'ItemName':'Computer',
+    'Price':'$300'},
+  ]
+const listItems = data.map((element)=>{return(<div className='h-[20rem] w-[14rem] group '>
+<div className=''> <img className=' hover:object-center hover:border-solid hover:border-2 group-hover:border-yellow-400' src={img} alt='' /></div>
+<div> <p className='text-lg text-blue-500 font-semibold'>{element.ItemName}</p>
+  <div className='flex justify-between m-3 opacity-0 group-hover:opacity-100 '>
+    <p className='text-lg text-blue-500'>{element.Price}</p>
+    <div className='flex '>
+      <FavouriteButton className='mr-2' />
+      <BuyButton className='' />
     </div>
-</a>
+  </div>
+</div>
+</div>
+)})
+  return (
+ <div className='flex'>{listItems}</div>
   )
+  
 }
 
 export default Card
