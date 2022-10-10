afb824344622dd7eefcac834c71fe36695ec5cb7
aasthan1206 <aastha.nayak2002@gmail.com>
2022-10-10 19:01:45 +0530

    no change

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,6 @@ import Button from "../components/shared/Button"
 const Contact = () => {
 
     const fieldStyle = "w-96 my-4 px-2 py-1 bg-transparent border-2"
-    const btnStyle = "border-none bg-gold-web-golden text-dark-charcoal font-bold rounded-2xl my-4 px-[16px] py-[2px] hover:text-gold-web-golden hover:bg-dark-charcoal"
     const [text, setText] = useState('')
     
   return (
@@ -16,6 +15,7 @@ const Contact = () => {
             <div className="text-white opacity-30 text-9xl font-bold relative -z-1">CONTACT</div>
             <div className="text-gold-web-golden text-5xl font-bold absolute z-1 inset-12">GET IN TOUCH</div>
         </div>
+
         <div className="flex m-12 mb-0 gap-16 justify-around">
             <div className="w-full">
                 <div className="text-white text-2xl font-semibold my-4">FOR QUERIES</div>
@@ -34,13 +34,14 @@ const Contact = () => {
             <form className="my-8 w-full">
                 {/* Mapping Form Fields */}
                 {/* Bug */}
-                {FormFields.map((fields) => (
-                    <input type={fields.type} placeholder={fields.placeholder} className={fieldStyle} value={text} onChange={(e) => setText(e.target.value)}/>
-                ))}
-                <Button value="Submit" className={btnStyle}/>
+                
+                <Button value="Submit" className="border-none bg-gold-web-golden text-dark-charcoal font-bold rounded-2xl my-4 px-[16px] py-[2px] hover:text-gold-web-golden hover:bg-[#2f2f2f]"/>
             </form>
             
         </div>
+        {FormFields.map((fields) => (
+                    <input type={fields.type} placeholder={fields.placeholder} className={fieldStyle} value={text} onChange={(e) => setText(e.target.value)}/>
+                ))}
     </div>
   )
 }
--- src/utils/FormFields.js
+++ src/utils/FormFields.js
@@ -4,14 +4,14 @@ export const FormFields = [
         'type': 'input',
         'placeholder': 'Enter your name',
     },
-    {
-        'id': 301,
-        'type': 'input',
-        'placeholder': 'Enter your email',
-    },
-    {
-        'id': 301,
-        'type': 'textarea',
-        'placeholder': 'Enter your message',
-    },
+    // {
+    //     'id': 301,
+    //     'type': 'input',
+    //     'placeholder': 'Enter your email',
+    // },
+    // {
+    //     'id': 301,
+    //     'type': 'textarea',
+    //     'placeholder': 'Enter your message',
+    // },
 ]
