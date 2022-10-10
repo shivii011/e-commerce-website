b24f38821adaeef2266b2bb33499527880db7992
shivii011 <shiv011kush011@gamil.com>
2022-10-11 02:04:38 +0530

    [sk]-making dinamic using array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Footer from './components/shared/Footer';
 
 function App() {
   return (
     <div className="App">
      E commerce
+     <Footer/>
     </div>
   );
 }
