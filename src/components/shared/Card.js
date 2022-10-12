e0b2ae6059e93ed4b1e74fcf69671dcb70f29b26
shivii011 <shiv011kush011@gamil.com>
2022-10-13 02:52:39 +0530

    [sk]-making responsive

--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -6,17 +6,18 @@ import { Data } from '../../utils/data';
 
 const Card = (className) => {
   return (
-    <div className='h-[20rem] w-[14rem] group'>
+    <div className='group'>
       {Data.map((data) => (
-          <div className={`group-hover:shadow-lg ${className}`}>
-            <div className=''><img className=' h-[14rem]hover:object-center hover:border-solid hover:border-2 group-hover:border-yellow-400' src={data.image} alt='' /></div>
-            <div>
-              <p className='text-lg text-blue-500 font-semibold'>{data.ItemName}</p>
-              <div className='flex justify-between m-3 p-2 opacity-0 group-hover:opacity-100 '>
-                  <p className='text-lg text-blue-500'>{data.Price}</p>
-                  <div className='flex '>
-                    <FavouriteButton className='mr-2' />
-                    <BuyButton className='' />
+          <div className={`hover:shadow-lg flex ${className}`}>
+            <div className='w-2/5'><img className=' hover:object-center hover:border-solid hover:border-2 group-hover:border-yellow-400' src={data.image} alt='' /></div>
+            <div className=' text-left pl-5 w-3/5'>
+              <p className='text-md text-dark-gray font-semibold '>{data.ItemType}</p>
+              <p className='text-2xl text-dark-gray font-semibold'>{data.ItemName}</p>
+              <div className='opacity-100 group-hover:opacity-100 '>
+                  <p className='text-2xl text-dark-gray'>{data.Price}</p>
+                  <div className='flex'>
+                    <FavouriteButton className='mx-2' /><p  className='text-gray'>Wishlist</p>
+                    <BuyButton className='mx-2' /><p className='text-gray'>Add To Cart</p>
                   </div>
                 </div>
             </div>
