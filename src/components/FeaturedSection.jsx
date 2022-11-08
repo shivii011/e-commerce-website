6b7df929029a802abc79909ace2d627e7ab18dcc
aasthan1206 <aastha.nayak2002@gmail.com>
2022-11-09 00:58:48 +0530

    moving use effect in featured section

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -6,12 +6,26 @@ import "swiper/css/pagination";
 import {Pagination} from "swiper";
 import Card from './shared/Card'
 
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function FeaturedSection(Slides) {
+    const [products, setProducts] = useState([])
+  useEffect(() => {
+    axios
+      .get(`https://dp-backend-e-comm.herokuapp.com/api/products`)
+      .then((res) => {
+        let { data } = res.data;
+        let _data = data.slice(0, 6);
+        setProducts(_data);
+      })
+  }, [])
+
     Slides = 1;
     return (
         <div className="">
-            <p className="text-center text-2xl font-semibold text-blue-500 ">Featured Section</p>
+            <p className="text-center text-5xl font-semibold text-blue mt-12 ">Featured Section</p>
             <Swiper slidesPerView={1}
                 centeredSlides={true}
                 spaceBetween={30}
@@ -25,7 +39,7 @@ export default function FeaturedSection(Slides) {
                 className="m-10"
             >
                 <SwiperSlide className="">
-                    <Card />
+                    <Card products={products}/>
                 </SwiperSlide>
             </Swiper>
         </div>
--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -1,35 +1,48 @@
-import React from 'react';
-import BuyButton from './BuyButton';
-import FavouriteButton from './FavouriteButton';
-import { Data } from '../../utils/data';
+import React from "react";
+import BuyButton from "./BuyButton";
+import FavouriteButton from "./FavouriteButton";
 
-const Card = (className) => {
+
+const Card = ({ className, products }) => {
   return (
-    <div className=' max-w-[14rem] my-[3rem] bg-gray rounded-xl font-main shadow-md hover:shadow-xl hover:transform duration-75 hover:scale-105 '>
-      {Data.map((data) => (
-          <div className={` transition-shadow ${className}`}>
-                <div className='absolute z-10 '> <FavouriteButton className='p-2 rounded-full w-10 h-10 mx-[10.86rem] mt-2 text-blue  border'/></div>
-            <div className=' max-w-[13] border-1 relative p-6'><img className='rounded-lg' src={data.image} alt='' /></div>
-            <div className='pl-3 pb-2'>
-              <p className='text-md text-zinc-500'>{data.ItemType}</p>
-              <p className='text-xl font-semibold py-1'>{data.ItemName}</p>
-              <div className='opacity-100 group-hover:opacity-100 flex '>
-                  <p className=' px-1 text-lg'>{data.Discounted}</p>
-                  <p className=' px-1 text-sm text-zinc-500 '><s>{data.Price}</s></p>
-                  {/* <p className=' px-1  text-xs text-green'>{data.DiscountPercentage}</p> */}
-                  
-                </div>
-                <div className='flex  px-1 '>
-                    <div className='bg-green rounded-lg flex p-1 cursor-pointer'><p className='text-white'>{data.rating}</p><i className="fa fa-star text-white text-xs pt-1"/> </div>
-                    <div className='bg-orange rounded-lg flex p-1 ml-8 cursor-pointer text-sm '><p className='text-white'>Add to cart</p></div>
-                    
-                  </div>
+    <div className="my-4 rounded-xl font-main px-12 gap-8 flex flex-wrap justify-center">
+      {products.map((product) => (
+        <div
+          className={` transition-shadow bg-gray hover:transform duration-75 hover:scale-105 w-80 shadow-md hover:shadow-xl  ${className}`}
+        >
+          <div className="flex justify-end">
+            {" "}
+            <FavouriteButton className="p-2 rounded-full w-10 h-10 mt-4 text-blue border mr-4" />
+          </div>
+          <div className="border-1 p-6">
+            <img
+              className="rounded-lg h-40 mx-auto"
+              src={product.productImages[0].productImageUrl}
+              alt=""
+            />
+          </div>
+          <div className="pl-3 pb-2">
+            {/* <p className='text-md text-zinc-500'>{product.productName}</p> */}
+            <p className="text-xl font-semibold py-1">{product.productName}</p>
+            <div className="flex ">
+              <p className=" px-1 text-lg">{product.discountedPrice}</p>
+              <p className=" px-1 text-sm text-zinc-500 ">
+                <s>{product.originalPrice}</s>
+              </p>
+              {/* <p className=' px-1  text-xs text-green'>{data.DiscountPercentage}</p> */}
+            </div>
+            <div className="flex  px-1 ">
+              {/* <div className='bg-green rounded-lg flex p-1 cursor-pointer'><p className='text-white'>{data.rating}</p><i className="fa fa-star text-white text-xs pt-1"/> </div> */}
+              <div className="bg-orange rounded-lg flex py-2 px-3 cursor-pointer text-sm">
+                <p className="text-white">Add to cart</p>
+              </div>
             </div>
           </div>
+        </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
 // <BuyButton className='mx-2 text-white' />
