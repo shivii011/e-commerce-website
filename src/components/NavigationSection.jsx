b926d9ef58441528ac44c4789422cc212a9cec69
shivii011 <shiv011kush011@gamil.com>
2022-11-09 20:20:19 +0530

    [sk]-renderig data

--- src/components/NavigationSection.jsx
+++ src/components/NavigationSection.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React ,{useEffect,useState}from 'react';
+import axios from 'axios';
 import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/css/pagination";
 import {Pagination} from "swiper";
-import Card from './shared/Card';
+import Card from './shared/NavigationCard';
 import 'antd/dist/antd.min.css'
 import { Tabs } from 'antd';
 import './NavigationSection.css'
 
 function NavigationSection() {
+    const [productdata,setProductdata]=useState([])
+    const [productdata2,setProductdata2]=useState([])
+    const [productdata3,setProductdata3]=useState([])
+useEffect(()=>{
+  axios
+  .get("https://dp-backend-e-comm.herokuapp.com/api/products")
+  .then((response)=>{
+    let {data} = response.data
+    let _data=data.slice(0,2)
+    let _data2=data.slice(2,4)
+    let _data3=data.slice(4,6)
+    setProductdata2(_data2)
+    setProductdata3(_data3)
+    setProductdata(_data)})
+},[])
   return (
   <div className='text-center font-main'>
  <Tabs  defaultActiveKey="1" >
@@ -25,7 +41,7 @@ function NavigationSection() {
                 className="m-10"
             >
                 <SwiperSlide>
-                <Card /> 
+                <Card className="grid-flow-col" productdata={productdata3}/> 
                 </SwiperSlide>
             </Swiper>
     </Tabs.TabPane>
@@ -43,7 +59,7 @@ function NavigationSection() {
                 className="m-10"
             >
                 <SwiperSlide>
-                <Card /> 
+                <Card productdata={productdata2}/> 
                 </SwiperSlide>
             </Swiper>
     </Tabs.TabPane>
@@ -61,7 +77,7 @@ function NavigationSection() {
                 className="m-10"
             >
                 <SwiperSlide>
-                <Card />
+                <Card productdata={productdata} />
                 </SwiperSlide>
             </Swiper>
     </Tabs.TabPane>
