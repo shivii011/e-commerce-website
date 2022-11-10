e04a6ca14bc76fca63db67ae8f46a6d3284ef322
shivii011 <shiv011kush011@gamil.com>
2022-11-10 17:16:41 +0530

    [sk]-adding login page with aoth0

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { Auth0Provider } from "@auth0/auth0-react";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<Auth0Provider
+domain="dev-avkouj633nzvb1zr.us.auth0.com"
+clientId="ft2Df1n1UyV5Hti0KVZR2YyL6kJg65fy"
+redirectUri={window.location.origin}
+>
+<React.StrictMode>
     <App />
   </React.StrictMode>
+</Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
